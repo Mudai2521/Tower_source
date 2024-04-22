87454d468264803f38026dec92383e6ed416170f
Mudai2521 <i.naka001@gmail.com>
2024-04-22 09:05:04 +0900

    a
    a

--- D3D12Application.cpp
+++ D3D12Application.cpp
@@ -184,6 +184,7 @@ void D3D12Application::LoadPipeline()
     swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
     swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
     swapChainDesc.SampleDesc.Count = 1;
+    swapChainDesc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_TEARING;
 
     ComPtr<IDXGISwapChain1> swapChain;
     ThrowIfFailed(factory->CreateSwapChainForHwnd(
